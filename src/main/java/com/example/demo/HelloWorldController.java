c5bf9173f8c62cde8f78af9534f89d4185be9d8a
Saahil Shah <saahil18105@iiitd.ac.in>
2020-06-15 01:59:14 +0530

    modified controller to add greeting message

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -2,22 +2,28 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
-@RequestMapping("api/hello-world")
+//@RequestMapping("api/hello-world")
 @RestController
 public class HelloWorldController {
+    private static final String template = "Hello, %s!";
+    private static AtomicLong counter = new AtomicLong();
 
-    @GetMapping
-    public List<String> getHelloWorld(){
-        List<String > lst= new ArrayList<String>();
-        lst.add("Hello");
-        lst.add("Saahil");
-        //return List.of("Hello","World");
-        return lst;
-
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name",defaultValue = "World") String name){
+        return new Greeting(counter.incrementAndGet(),String.format(template,name));
     }
+//    public List<String> getHelloWorld(){
+//        List<String > lst= new ArrayList<String>();
+//        lst.add("Hello");
+//        lst.add("Saahil");
+//        //return List.of("Hello","World");
+//        return lst;
+//    }
 }
